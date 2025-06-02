3648fd48357d48b295fb94c8fa46b997ca983a2c
Ivan Korsunov <ivan.corsunov@gmail.com>
2025-06-02 14:23:01 +0300

    add mqtt example

--- examples/mqtt_nosys/w5x00_mqtt_nosys.c
+++ examples/mqtt_nosys/w5x00_mqtt_nosys.c
@@ -18,15 +18,19 @@
 #include "w5x00_spi.h"
 #include "w5x00_lwip.h"
 
+#include "lwip/apps/mqtt.h"
+
 #include "lwip/init.h"
 #include "lwip/netif.h"
 #include "lwip/timeouts.h"
 
 #include "lwip/apps/lwiperf.h"
 #include "lwip/etharp.h"
+#include "lwip/dhcp.h"
+#include "lwip/dns.h"
 
-#include "tcp_echoserver.h"
-#include "tcp_echoclient.h"
+#include <string.h>
+#include "hardware/adc.h"
 
 /**
  * ----------------------------------------------------------------------------------------------------
@@ -42,10 +46,6 @@
 /* Port */
 #define PORT_LWIPERF 5001
 
-/* TCP mode */
-//#define SERVER
-#define CLIENT
-
 /**
  * ----------------------------------------------------------------------------------------------------
  * Variables
@@ -53,12 +53,34 @@
  */
 /* Network */
 extern uint8_t mac[6];
-static ip_addr_t g_ip;
-static ip_addr_t g_mask;
-static ip_addr_t g_gateway;
 
 /* LWIP */
 struct netif g_netif;
+
+/* DNS */
+static uint8_t g_dns_target_domain[] = "www.wiznet.io";
+static uint8_t g_dns_get_ip_flag = 0;
+static uint32_t g_ip;
+static ip_addr_t g_resolved;
+
+/* MQTT */
+static mqtt_client_t *mqtt_client;
+static const struct mqtt_connect_client_info_t mqtt_client_info =
+    {
+        "test",
+        NULL, /* user */
+        NULL, /* pass */
+        100,  /* keep alive */
+        NULL, /* will_topic */
+        NULL, /* will_msg */
+        0,    /* will_msg_len */
+        0,    /* will_qos */
+        0     /* will_retain */
+#if LWIP_ALTCP && LWIP_ALTCP_TLS
+        ,
+        NULL
+#endif
+};
 
 /**
  * ----------------------------------------------------------------------------------------------------
@@ -67,6 +89,11 @@ struct netif g_netif;
  */
 /* Clock */
 static void set_clock_khz(void);
+
+static void mqtt_connection_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t status);
+static void mqtt_pub_request_cb(void *arg, err_t result);
+void init_mqtt();
+float read_temperature();
 
 /**
  * ----------------------------------------------------------------------------------------------------
@@ -80,11 +107,6 @@ int main()
     uint8_t *pack = malloc(ETHERNET_MTU);
     uint16_t pack_len = 0;
     struct pbuf *p = NULL;
-
-    // Initialize network configuration
-    IP4_ADDR(&g_ip, 192, 168, 11, 2);
-    IP4_ADDR(&g_mask, 255, 255, 255, 0);
-    IP4_ADDR(&g_gateway, 192, 168, 11, 1);
 
     set_clock_khz();
 
@@ -104,12 +126,22 @@ int main()
     setSHAR(mac);
     ctlwizchip(CW_RESET_PHY, 0);
 
+    adc_init();
+    adc_set_temp_sensor_enabled(true);
+    adc_select_input(0);
+
     // Initialize LWIP in NO_SYS mode
     lwip_init();
 
-    netif_add(&g_netif, &g_ip, &g_mask, &g_gateway, NULL, netif_initialize, netif_input);
+    netif_add(&g_netif, IP4_ADDR_ANY, IP4_ADDR_ANY, IP4_ADDR_ANY, NULL, netif_initialize, netif_input);
     g_netif.name[0] = 'e';
     g_netif.name[1] = '0';
+
+    // ip_addr_t ip, nm, gw;
+    // IP4_ADDR(&ip, 192, 168, 1, 100);
+    // IP4_ADDR(&nm, 255, 255, 255, 0);
+    // IP4_ADDR(&gw, 192, 168, 1, 1);
+    // netif_set_addr(&g_netif, &ip, &nm, &gw);
 
     // Assign callbacks for link and status
     netif_set_link_callback(&g_netif, netif_link_callback);
@@ -124,17 +156,17 @@ int main()
     }
 
     // Set the default interface and bring it up
+    netif_set_default(&g_netif);
     netif_set_link_up(&g_netif);
     netif_set_up(&g_netif);
 
-#ifdef SERVER
-    // Start tcp echo server
-    tcp_echoserver_init();
-#else defined (CLIENT)
-    // Start tcp echo client
-    tcp_echoclient_init();
-#endif
+    // Start DHCP configuration for an interface
+    dhcp_start(&g_netif);
+
+    // dns_init();
+
     /* Infinite loop */
+
     while (1)
     {
         getsockopt(SOCKET_MACRAW, SO_RECVBUF, &pack_len);
@@ -166,9 +198,13 @@ int main()
                 }
             }
         }
-
         /* Cyclic lwIP timers check */
         sys_check_timeouts();
+
+        if ((g_netif.ip_addr.addr > 0) && (g_netif.netmask.addr > 0) && (g_netif.gw.addr > 0) && !mqtt_client)
+        {
+            init_mqtt();
+        }
     }
 }
 
@@ -177,6 +213,106 @@ int main()
  * Functions
  * ----------------------------------------------------------------------------------------------------
  */
+void init_mqtt()
+{
+    err_t err;
+    ip_addr_t broker_addr;
+    if (!ipaddr_aton(MQTT_BROKER, &broker_addr))
+    {
+        printf("Invalid broker IP\n");
+        return;
+    }
+    struct eth_addr pc_mac;
+    pc_mac.addr[0] = 0x8C;
+    pc_mac.addr[1] = 0xC6;
+    pc_mac.addr[2] = 0x81;
+    pc_mac.addr[3] = 0x87;
+    pc_mac.addr[4] = 0x06;
+    pc_mac.addr[5] = 0xE5;
+
+    err = etharp_add_static_entry(&broker_addr, &pc_mac);
+    if (err != ERR_OK)
+    {
+        printf("Failed to add ARP entry: %d\n", err);
+    }
+
+    struct mqtt_connect_client_info_t ci = {
+        .client_id = "pico_eth",
+        .keep_alive = 60,
+        .client_user = NULL,
+        .client_pass = NULL};
+
+    mqtt_client = mqtt_client_new();
+    if (!mqtt_client)
+    {
+        printf("Failed to create client\n");
+        return;
+    }
+
+    err = mqtt_client_connect(mqtt_client, &broker_addr, MQTT_PORT,
+                              mqtt_connection_cb, NULL, &ci);
+    if (err != ERR_OK)
+    {
+        printf("Connect error: %d\n", err);
+    }
+}
+
+static void mqtt_connection_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t status)
+{
+    const char *pub_payload = "HELLO, it's PICO";
+    err_t err;
+    u8_t qos = 0;    /* 0 1 or 2, see MQTT specification */
+    u8_t retain = 0; /* No don't retain such crappy payload... */
+    err = mqtt_publish(client, "pico", pub_payload, strlen(pub_payload), qos, retain, mqtt_pub_request_cb, arg);
+    if (err != ERR_OK)
+    {
+        printf("Publish err: %d\n", err);
+    }
+}
+
+// Callback после публикации
+static void mqtt_pub_request_cb(void *arg, err_t result)
+{
+    err_t err;
+    if (result == ERR_OK)
+    {
+        printf("Message published successfully\n");
+        sleep_ms(1000 * 3); // wait for 3 seconds
+        char pub_payload[10];
+        sprintf(pub_payload, "%f", read_temperature());
+        err_t err;
+        u8_t qos = 0;    /* 0 1 or 2, see MQTT specification */
+        u8_t retain = 0; /* No don't retain such crappy payload... */
+        err = mqtt_publish(mqtt_client, "pico", pub_payload, strlen(pub_payload), qos, retain, mqtt_pub_request_cb, NULL);
+        if (err != ERR_OK)
+        {
+            printf("Publish err: %d\n", err);
+        }
+    }
+    else
+    {
+        printf("Publish failed: %d\n", result);
+    }
+}
+
+float read_temperature()
+{
+    uint16_t raw = adc_read();
+    const float conversion_factor = 3.3f / (4096 - 1);
+    float result = raw * conversion_factor;
+    if (adc_get_selected_input() == 4)
+    {
+        
+        float temp = 27 - (result - 0.706)/0.001721;
+        return temp;
+    }
+    else
+    {
+        float temp = (result - 1.25)/0.005;
+        return temp;
+    }
+}
+
 /* Clock */
 static void set_clock_khz(void)
 {
--- port/lwip/lwipopts.h
+++ port/lwip/lwipopts.h
@@ -43,7 +43,12 @@
 #define LWIP_ICMP 1
 #define LWIP_UDP 1
 #define LWIP_TCP 1
-#define MEM_SIZE 2048
+#define LWIP_ARP                    1
+#define LWIP_ETHERNET               1
+#define MEM_SIZE 4096
+
+#define LWIP_IPV4 1
+#define LWIP_IPV6 0
 
 // disable ACD to avoid build errors
 // http://lwip.100.n7.nabble.com/Build-issue-if-LWIP-DHCP-is-set-to-0-td33280.html
@@ -55,8 +60,8 @@
 #define LWIP_NETIF_LINK_CALLBACK 1
 #define LWIP_NETIF_STATUS_CALLBACK 1
 
-#define TCP_MSS (1500 /*mtu*/ - 20 /*iphdr*/ - 20 /*tcphhr*/)
-#define TCP_SND_BUF (2 * TCP_MSS)
+// #define TCP_MSS (1500 /*mtu*/ - 20 /*iphdr*/ - 20 /*tcphhr*/)
+// #define TCP_SND_BUF (2 * TCP_MSS)
 
 #define LWIP_HTTPD_CGI 0
 #define LWIP_HTTPD_SSI 0
@@ -73,6 +78,45 @@
 #define TCPIP_DEBUG LWIP_DBG_OFF
 #define DHCP_DEBUG LWIP_DBG_OFF
 #define UDP_DEBUG LWIP_DBG_OFF
+#define MEM_DEBUG LWIP_DBG_OFF
+#define MQTT_DEBUG LWIP_DBG_ON
+#define NETIF_DEBUG LWIP_DBG_OFF
 #endif
+
+#define MQTT_BROKER "192.168.1.108"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "pico"
+#define MQTT_CLIENT_ID "pico_client"
+
+#define MEM_LIBC_MALLOC             0
+#define MEM_ALIGNMENT               4
+#define MEM_SIZE                    16000
+#define MEMP_NUM_TCP_SEG            32
+#define MEMP_NUM_ARP_QUEUE          20
+#define PBUF_POOL_SIZE              24
+#define TCP_WND                     16384
+#define MEMP_NUM_TCP_PCB             5 
+#define TCP_MSS                     1460
+#define TCP_TMR_INTERVAL            250
+#define ETH_PAYLOAD_SIZE            1500
+#define TCP_MSL                     60000   
+#define TCP_SND_BUF                 (8 * TCP_MSS)
+#define TCP_SND_QUEUELEN            ((4 * (TCP_SND_BUF) + (TCP_MSS - 1)) / (TCP_MSS))
+#define PBUF_POOL_BUFSIZE           1600
+#define LWIP_NETIF_STATUS_CALLBACK  1
+#define LWIP_NETIF_LINK_CALLBACK    1
+#define LWIP_NETIF_HOSTNAME         1
+#define LWIP_NETCONN                0
+#define MEM_STATS                   0
+#define SYS_STATS                   0
+#define MEMP_STATS                  0
+#define LINK_STATS                  0
+
+#define ARP_QUEUEING 1
+#define ARP_TABLE_SIZE 10
+#define ARP_MAXAGE 300
+#define ETHARP_SUPPORT_STATIC_ENTRIES 1
+
+#define MEMP_NUM_SYS_TIMEOUT        (LWIP_NUM_SYS_TIMEOUT_INTERNAL + 2)
 
 #endif /* __LWIPOPTS_H__ */
